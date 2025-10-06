947a826f93dec937515a6fb433723fa0989e231b
RdashJay <arjayconradez16@gmail.com>
2025-10-06 20:30:37 +0800

    Added Upcomming Itenerary

---
+++ project.thesis-main/src/pages/itineraries/UpcomingItinerary.tsx
@@ -0,0 +1,10 @@
+function UpcomingItinerary() {
+  return (
+    <div>
+      <h1>Upcoming Itinerary</h1>
+      <p>This is the Upcoming Itinerary page.</p>
+    </div>
+  );
+}
+
+export default UpcomingItinerary;
