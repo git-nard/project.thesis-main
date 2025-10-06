d17ef71e8294ca2dc92deb0d548b72d42d1c3264
RdashJay <arjayconradez16@gmail.com>
2025-10-06 20:30:21 +0800

    AddedRestaurantList

---
+++ project.thesis-main/src/pages/saved/SavedRestaurantsList.tsx
@@ -0,0 +1,10 @@
+function SavedRestaurantsList() {
+  return (
+    <div>
+      <h1>Saved Restaurants</h1>
+      <p>This is the Saved Restaurants page.</p>
+    </div>
+  );
+}
+
+export default SavedRestaurantsList;
