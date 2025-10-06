2587510d8fe7bdbe2584964ecda81b30626cd950
RdashJay <arjayconradez16@gmail.com>
2025-10-06 20:31:55 +0800

    tourist spot card changes

--- project.thesis-main/src/components/tourists-spots/TouristSpotsCard.tsx
+++ project.thesis-main/src/components/tourists-spots/TouristSpotsCard.tsx
@@ -48,6 +48,13 @@ const TouristSpotCard = ({
         >
           {category}
         </Badge>
+        <button className="group absolute top-3 left-3 bg-white w-10 h-10 rounded-full flex items-center justify-center transition-all duration-300 hover:bg-red-500">
+  <i className="fa-solid fa-bookmark text-red-500 transition-all duration-300 group-hover:text-white"></i>
+</button>
+
+
+
+
       </div>
 
       {/* Header */}
