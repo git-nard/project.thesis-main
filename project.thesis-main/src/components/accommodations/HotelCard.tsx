2d652856f1869d52d25503fab7f4102758f6ddb5
RdashJay <arjayconradez16@gmail.com>
2025-10-11 17:07:53 +0800

    added

--- project.thesis-main/src/components/accommodations/HotelCard.tsx
+++ project.thesis-main/src/components/accommodations/HotelCard.tsx
@@ -79,7 +79,12 @@ const HotelCard = ({
           <Badge variant="secondary" className="bg-white/90 text-black">
             {priceRange}
           </Badge>
+
+          
         </div>
+        <button className="group absolute top-3 left-3 bg-white w-10 h-10 rounded-full flex items-center justify-center transition-all duration-300 hover:bg-red-500">
+          <i className="fa-solid fa-bookmark text-red-500 transition-all duration-300 group-hover:text-white"></i>
+        </button>
       </div>
 
       {/* Info Section */}
